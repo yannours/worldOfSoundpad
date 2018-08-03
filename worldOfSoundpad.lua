ff2e60af47bcb97d668f8c8f95a0588876e2bdcb
yann-vai gouezou <nouyan@nouyan.home>
2018-08-03 12:00:26 +0200

    manage to play sound on spell cast, debug to identify spells and caster

--- worldOfSoundpad.lua
+++ worldOfSoundpad.lua
@@ -1,6 +1,16 @@
-local f = CreateFrame("Frame")
+local f = CreateFrame("Frame", "Wolrd Of Soundpad", UIParent);
 
-f:RegisterEvent("PLAYER_LOGIN")
-f:SetScript("OnEvent",function(self,event,...)
-	PlaySoundFile("Interface\\AddOns\\worldOfSoundpad\\sounds\\ownage.mp3")
-	end)
+f:RegisterEvent("PLAYER_LOGIN");
+f:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED");
+f:SetScript("OnEvent", function (self, event, ...)
+	if (event == "PLAYER_LOGIN") then
+		PlaySoundFile("Interface\\AddOns\\worldOfSoundpad\\sounds\\alarm.mp3");
+	end
+	if (event == "UNIT_SPELLCAST_SUCCEEDED") then
+		local caster, spell = select(1, ...), select(3, ...);
+		print(caster, spell);
+		if (spell == 79140) then
+			PlaySoundFile("Interface\\AddOns\\worldOfSoundpad\\sounds\\ownage.mp3");
+		end
+	end
+end)
